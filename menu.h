c5f0fa0987fa0f8b2328d6b377cae78cdd0c60e0
PaulDotSH <paulandreiabrudan@protonmail.com>
2022-04-12 10:57:10 -0500

    almost done

---
+++ menu.h
@@ -0,0 +1,14 @@
+//
+// Created by administrator on 4/12/22.
+//
+
+#ifndef SISTEM_BIBLIOTECA_CPP_MENU_H
+#define SISTEM_BIBLIOTECA_CPP_MENU_H
+
+
+class menu {
+
+};
+
+
+#endif //SISTEM_BIBLIOTECA_CPP_MENU_H
